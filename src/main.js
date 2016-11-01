dfb9a7f2286c40ebea8a7dd13ad4aaad7ec5a89a
Kaio Max <kaiomaks@gmail.com>
2016-11-01 19:19:39 -0300

    Init project

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Hello from './hello';
+
+const appRoot = document.getElementById('root');
+
+render(
+  <Hello/>,
+  appRoot
+);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const WebpackNotifierPlugin = require('webpack-notifier');
+
+// env
+const buildDirectory = './dist/';
+
+module.exports = {
+  entry: './src/main.js',
+  devServer: {
+    hot: true,
+    inline: true,
+    port: 8080,
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(buildDirectory),
+    filename: 'app.js',
+    publicPath: 'http://localhost:8080/dist',
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+        },
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ],
+  },
+  plugins: [
+    new WebpackNotifierPlugin()
+  ]
+};
